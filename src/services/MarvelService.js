e47ec390304b540f212532b1f1217761c6b502fb
Karyna-Ko <karina.korshak@gmail.com>
2022-08-29 12:14:18 +0300

    load more button

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@ class MarvelService {
 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/'
     _apiKey = 'apikey=6817aaabc15360765f77a68ccfe83d93'
+    _baseOffset = 210;
     getResource = async (url) => {
         let res = await fetch(url);
     
@@ -12,8 +13,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter)
     }
 

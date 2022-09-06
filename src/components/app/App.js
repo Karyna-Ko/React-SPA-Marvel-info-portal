aa1b328f3e0138c6859c351b64b449d4e003cc88
Karyna-Ko <karina.korshak@gmail.com>
2022-09-06 10:28:40 +0300

    navigation react router,second page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,8 @@
-import { useState } from "react";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from '../../resources/img/vision.png';
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
+import {MainPage, ComicsPage} from "../pages";
 
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null);    
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
 
     return (
         <Router>
@@ -26,26 +11,13 @@ const App = () => {
              <main>
                <Switch>
                 <Route exact path="/">
-                        <ErrorBoundary>
-                            <RandomChar/>
-                        </ErrorBoundary>
-                        
-                        <div className="char__content">
-                            <ErrorBoundary>
-                            <CharList onCharSelected={onCharSelected}/>
-                            </ErrorBoundary>
-                            
-                            <ErrorBoundary>
-                                <CharInfo charId={selectedChar}/>
-                            </ErrorBoundary>
-                        </div>
-                        <img className="bg-decoration" src={decoration} alt="vision"/>
-                    </Route>
+                    <MainPage /> 
+                </Route>
 
-                    <Route exact path="/comics">
-                        <AppBanner />
-                        <ComicsList />
-                    </Route>
+                <Route exact path="/comics">
+                    <ComicsPage />
+                </Route>
+
                </Switch>
             </main>
         </div>
--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import './style/style.scss';
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render( 
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
   );
 
